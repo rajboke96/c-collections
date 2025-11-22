1c168546d3547e80e2d9ce2cc99aeb44952fbbad
rajboke96 <rajendra.boke08@gmail.com>
2025-11-22 19:27:18 +0530

    added Ds_DNode for dll

--- include/node.h
+++ include/node.h
@@ -9,3 +9,15 @@ void Ds_Node__new__(void *data, Ds_Node *next);
 void Ds_Node__init__(Ds_Node *self, void *data, Ds_Node *next);
 void Ds_Node__free__(Ds_Node *self);
 #endif
+#ifndef DNODE_DEF
+#define DNODE_DEF
+typedef struct Ds_DNode{
+    void *data;
+    struct Ds_DNode *prev;
+    struct Ds_DNode *next;
+} Ds_DNode;
+
+void Ds_DNode__new__(void *data, Ds_DNode *next);
+void Ds_DNode__init__(Ds_DNode *self, void *data, Ds_DNode *next);
+void Ds_DNode__free__(Ds_DNode *self);
+#endif
