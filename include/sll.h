0eaf45a799b21cc034c844666209e9973e157c1c
rajboke96 <rajendra.boke08@gmail.com>
2025-11-22 18:05:46 +0530

    Update sll.h

--- include/sll.h
+++ include/sll.h
@@ -15,9 +15,13 @@ void Ds_SLL__init__(Ds_SLL *self, int dt_size);
 // destructor function
 void Ds_SLL__free__(Ds_SLL *self);
 // operation functions
-int Ds_SLL__insert(Ds_SLL *self, int data);
-Ds_Node *Ds_SLL__search(Ds_SLL *self, int data);
-int Ds_SLL__delete(Ds_SLL *self);
+int Ds_SLL__insert_at_first(Ds_SLL *self, void *data);
+int Ds_SLL__insert_at_last(Ds_SLL *self, void *data);
+Ds_Node *Ds_SLL__search(Ds_SLL *self, void *data);
+int Ds_SLL__insert_after(Ds_SLL *self, Ds_Node *tmp_node, void *data);
+void *Ds_SLL__delete_at_first(Ds_SLL *self);
+void *Ds_SLL__delete_at_last(Ds_SLL *self);
+void *Ds_SLL__delete(Ds_SLL *self, void *data);
 const Ds_Array *Ds_traverse(Ds_SLL *self);
 int Ds_SLL__get_size(Ds_SLL *self);
 #endif
