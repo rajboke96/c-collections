a0f47864037407e7920d804960df0104a2b140f0
rajboke96 <rajendra.boke08@gmail.com>
2025-11-23 11:58:27 +0530

    implementing sll

--- src/sll.c
+++ src/sll.c
@@ -5,30 +5,46 @@
 #include<array.h>
 
 // constructor function
-Ds_sll__SLL *Ds_sll__new_SLL(){
-    
+Ds_SLL *Ds_SLL__new__(int dt_size){
+    Ds_SLL *self=(DS_SLL *)malloc(DS_SLL);
+    if(self==NULL) return NULL;
+    Ds_SLL__init__(self, dt_size);
 }
 // init function
-void Ds_sll__init_SLL(Ds_sll__SLL *self){
-    
+void Ds_SLL__init__(Ds_SLL *self, int dt_size){
+    self->dt_size=dt_size;
+    self->size=0;
 }
-// representation function
-char *Ds_sll__get_str(Ds_sll__SLL *self){
-    
+// destructor function
+void Ds_SLL__free__(Ds_SLL *self){
+    if(self!=NULL){
+        for(int i=0;i<self->size;i++){
+           Ds_Node__delete_at_first(self);
+        }
+        free(self);
+    }
 }
 // operation functions
-int Ds_sll__insert(Ds_sll__SLL *self, int data){
-    
+int Ds_SLL__insert_at_first(Ds_SLL *self, void *data);
+int Ds_SLL__insert_at_last(Ds_SLL *self, void *data){
+    Ds_Node *tail=self->head;
+    while(tail !=NULL){
+        if(tail->next==NULL) break;
+        tail=tmp->next;
+    }
+    Ds_Node *new_node=Ds_Node__new__(self->dt_size, data, NULL);
+    if(tail == NULL){
+        self->head=new_node;
+    } else{
+        tail->next=new_node;
+    }
 }
-Ds_Node *Ds_sll__search(Ds_sll__SLL *self, int data){
-    
-}
-int Ds_sll__delete(Ds_sll__SLL *self){
-    
-}
-const Ds_Array *Ds_traverse(Ds_sll__SLL *self){
-    
-}
-int Ds_sll__get_size(Ds_sll__SLL *self){
-    
+Ds_Node *Ds_SLL__search(Ds_SLL *self, void *data);
+int Ds_SLL__insert_after(Ds_SLL *self, Ds_Node *tmp_node, void *data);
+void *Ds_SLL__delete_at_first(Ds_SLL *self);
+void *Ds_SLL__delete_at_last(Ds_SLL *self);
+void *Ds_SLL__delete(Ds_SLL *self, void *data);
+const Ds_Array *DS_SLL__traverse(Ds_SLL *self);
+int Ds_SLL__get_size(Ds_SLL *self){
+    return self->size;
 }
